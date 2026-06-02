726e1a199cd64a7c64f50aef8ff9649cf6efbc80
abdou <abdououni67@google.com>
2026-06-02 10:30:10 +0100

    modified:   lib/core/services/call_detector.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- otp_protection/lib/core/services/call_detector.dart
+++ otp_protection/lib/core/services/call_detector.dart
@@ -1,7 +1,26 @@
+import 'dart:async';
+import 'package:phone_state/phone_state.dart';
 
 class CallDetector {
-  
-  Future<bool> isUserInCall() async {
-     return  true;
+  bool _isInCall = false;
+  StreamSubscription<PhoneState>? _subscription;
+
+  /// Call this ONCE when app starts
+  void init() {
+    _subscription = PhoneState.stream.listen((state) {
+      _isInCall = _isActiveCall(state.status);
+    });
+  }
+
+  bool get isUserInCall => _isInCall;
+
+  bool _isActiveCall(PhoneStateStatus status) {
+    return status == PhoneStateStatus.CALL_STARTED ||
+        status == PhoneStateStatus.CALL_INCOMING;
+  }
+
+  /// Call this when app is disposed (important for cleanup)
+  void dispose() {
+    _subscription?.cancel();
   }
 }
